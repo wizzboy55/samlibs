59e705e643b6fc562521b96b97364d325693e365
charlesgervais <charles.gervais.dumont@gmail.com>
2020-01-04 15:06:44 -0500

    SercomPad now returns -1 on invalid pad selection

--- comm/sercom.c
+++ comm/sercom.c
@@ -8,29 +8,28 @@
 #include "sercom.h"
 #include "FreeRTOS.h"
 
-uint8_t xSercomPadToRXPO(enum SercomPads_e pad) {
+int8_t xSercomPadToRXPO(enum SercomPads_e pad) {
 	switch(pad) {
 		case PAD0:
-		return 0;
+			return 0;
 		case PAD1:
-		return 1;
+			return 1;
 		case PAD2:
-		return 2;
+			return 2;
 		case PAD3:
-		return 3;
+			return 3;
 		default:
-		return 0;
+			return -1;
 	}
 }
 
-uint8_t xSercomPadToTXPO(enum SercomPads_e pad) {
+int8_t xSercomPadToTXPO(enum SercomPads_e pad) {
 	switch(pad) {
 		case PAD0:
 			return 0;
 		case PAD2:
 			return 1;
 		default:
-			configASSERT(0);
-		return 0;
+			return -1;
 	}
 }
--- comm/sercom.h
+++ comm/sercom.h
@@ -13,7 +13,7 @@
 
 enum SercomPads_e {PAD0, PAD1, PAD2, PAD3};
 
-uint8_t xSercomPadToRXPO(enum SercomPads_e pad);
-uint8_t xSercomPadToTXPO(enum SercomPads_e pad);
+int8_t xSercomPadToRXPO(enum SercomPads_e pad);
+int8_t xSercomPadToTXPO(enum SercomPads_e pad);
 
 #endif /* SERCOM_H_ */
