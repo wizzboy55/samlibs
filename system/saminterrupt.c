2b57728a3771548e512c752669120aa5dd4926df
charlesgervais <charles.gervais.dumont@gmail.com>
2020-01-23 16:04:35 -0500

    Add support for External Interrupt Controller (EIC)

--- system/saminterrupt.c
+++ system/saminterrupt.c
@@ -6,29 +6,11 @@
  */ 
 
 #include "saminterrupt.h"
+#include "samclk.h"
 
 IRQn_Type sam_getModuleInterrupt(void* const module) {
 	uint32_t addr = (uint32_t)module;
-	#ifdef SAME54
-		switch(addr) {
-			case (uint32_t)SERCOM0:
-				return SERCOM0_IRQn;
-			case (uint32_t)SERCOM1:
-				return SERCOM1_IRQn;
-			case (uint32_t)SERCOM2:
-				return SERCOM2_IRQn;
-			case (uint32_t)SERCOM3:
-				return SERCOM3_IRQn;
-			case (uint32_t)SERCOM4:
-				return SERCOM4_IRQn;
-			case (uint32_t)SERCOM5:
-				return SERCOM5_IRQn;
-			case (uint32_t)SERCOM6:
-				return SERCOM6_IRQn;
-			case (uint32_t)SERCOM7:
-				return SERCOM7_IRQn;
-		}
-	#elif defined SAME53
+	#if defined(SAME54) || defined(SAME53)
 		switch(addr) {
 			case (uint32_t)SERCOM0:
 				return SERCOM0_0_IRQn;
@@ -42,6 +24,14 @@ IRQn_Type sam_getModuleInterrupt(void* const module) {
 				return SERCOM4_0_IRQn;
 			case (uint32_t)SERCOM5:
 				return SERCOM5_0_IRQn;
+			#ifdef SERCOM6
+			case (uint32_t)SERCOM6:
+				return SERCOM6_0_IRQn;
+			#endif
+			#ifdef SERCOM7
+			case (uint32_t)SERCOM7:
+				return SERCOM7_0_IRQn;
+			#endif
 		}
 	#elif defined SAMD20
 		switch(addr) {
@@ -82,4 +72,27 @@ void sam_enableModuleInterrupt(void* const module) {
 
 void sam_disableModuleInterrupt(void* const module) {
 	NVIC_DisableIRQ(sam_getModuleInterrupt(module));
+}
+
+void sam_EICInit(uint8_t clksource) {
+	samclk_enable_gclk_channel(EIC, clksource);
+	samclk_enable_peripheral_clock(EIC);
+	EIC->CTRLA.bit.ENABLE = 1;
+}
+
+inline void sam_EICEnableExtIRQ(eEICChannels_t channel) {
+	EIC->INTENSET.bit.EXTINT = (1<<channel);
+}
+
+inline void sam_EICDisableExtIRQ(eEICChannels_t channel) {
+	EIC->INTENCLR.bit.EXTINT = (1<<channel);
+}
+
+void sam_EICConfigExt(eEICChannels_t channel, BaseType_t debouce, BaseType_t filter, eEICSense_t sense) {
+	EIC->DEBOUNCEN.bit.DEBOUNCEN |= ((debouce == pdTRUE) << channel);
+	if(channel < EIC8) {
+		EIC->CONFIG[0].reg = ((((filter == pdTRUE) << 3) | (sense & 0x07)) & 0x0F) << (channel * 4);
+	} else {
+		EIC->CONFIG[1].reg = ((((filter == pdTRUE) << 3) | (sense & 0x07)) & 0x0F) << ((channel - EIC8) * 4);
+	}
 }
--- system/saminterrupt.h
+++ system/saminterrupt.h
@@ -9,13 +9,22 @@
 #ifndef SAMINTERRUPT_H_
 #define SAMINTERRUPT_H_
 
+#include "FreeRTOS.h"
+
 #include <hpl_pm_base.h>
 #include <hpl_gclk_base.h>
 #if defined(SAME54)
 	#include <hri_gclk_e54.h>
 #endif
 
+typedef enum {EIC0 = 0, EIC1, EIC2, EIC3, EIC4, EIC5, EIC6, EIC7, EIC8, EIC9, EIC10, EIC11, EIC12, EIC13, EIC14, EIC15} eEICChannels_t;
+typedef enum {EIC_None = 0, EIC_Rise, EIC_Fall, EIC_Both, EIC_High, EIC_Low} eEICSense_t;
+
 void sam_enableModuleInterrupt(void* const module);
 void sam_disableModuleInterrupt(void* const module);
+void sam_EICInit(uint8_t clksource);
+void sam_EICEnableExtIRQ(eEICChannels_t channel);
+void sam_EICDisableExtIRQ(eEICChannels_t channel);
+void sam_EICConfigExt(eEICChannels_t channel, BaseType_t debouce, BaseType_t filter, eEICSense_t sense);
 
 #endif /* SAMINTERRUPT_H_ */
