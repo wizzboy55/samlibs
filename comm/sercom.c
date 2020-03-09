7f081535a8747aebf7cf5bd6cf46057f2b3a54fa
charlesgervais <charles.gervais.dumont@gmail.com>
2020-03-09 16:52:21 -0400

    SERCOM: Add GPIO to PAD helper

--- comm/sercom.c
+++ comm/sercom.c
@@ -7,6 +7,42 @@
 
 #include "sercom.h"
 #include "FreeRTOS.h"
+#include "samgpio.h"
+
+enum SercomPads_e eSercomGPIOToPad(uint32_t gpio) {
+#if defined(SAMC20)
+	switch(gpio) {
+		case GPIO(GPIO_PORTA, 0):
+		case GPIO(GPIO_PORTA, 4):
+		case GPIO(GPIO_PORTA, 8):
+		case GPIO(GPIO_PORTA, 16):
+		case GPIO(GPIO_PORTA, 22):
+			return PAD0;
+		case GPIO(GPIO_PORTA, 1):
+		case GPIO(GPIO_PORTA, 5):
+		case GPIO(GPIO_PORTA, 9):
+		case GPIO(GPIO_PORTA, 17):
+		case GPIO(GPIO_PORTA, 23):
+			return PAD1;
+		case GPIO(GPIO_PORTA, 6):
+		case GPIO(GPIO_PORTA, 10):
+		case GPIO(GPIO_PORTA, 14):
+		case GPIO(GPIO_PORTA, 18):
+		case GPIO(GPIO_PORTA, 24):
+		case GPIO(GPIO_PORTA, 30):
+			return PAD2;
+		case GPIO(GPIO_PORTA, 7):
+		case GPIO(GPIO_PORTA, 11):
+		case GPIO(GPIO_PORTA, 15):
+		case GPIO(GPIO_PORTA, 19):
+		case GPIO(GPIO_PORTA, 25):
+		case GPIO(GPIO_PORTA, 31):
+			return PAD3;
+		default:
+			return -1;
+	}
+#endif
+}
 
 int8_t xSercomPadToRXPO(enum SercomPads_e pad) {
 	switch(pad) {
--- comm/sercom.h
+++ comm/sercom.h
@@ -13,6 +13,7 @@
 
 enum SercomPads_e {PAD0, PAD1, PAD2, PAD3};
 
+enum SercomPads_e eSercomGPIOToPad(uint32_t gpio);
 int8_t xSercomPadToRXPO(enum SercomPads_e pad);
 int8_t xSercomPadToTXPO(enum SercomPads_e pad);
 
